3e046566cf5457ff7380feaa0be041dbcd8d8b49
Envy <tranthanhtung1805@gmail.com>
2019-03-26 09:12:46 +0700

    Fix cứng listView - Listview bài đăng - gán vào report

--- app/src/main/java/com/example/zeroc/fitness/FragmentTrainingButton1.java
+++ app/src/main/java/com/example/zeroc/fitness/FragmentTrainingButton1.java
@@ -10,15 +10,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentTrainingButton1 extends Fragment {
 
-    RecyclerView mRecyclerView ;
-    RecyclerViewAdapter mRecyclerViewAdapter;
-    private List<String> data ;
+    ListView listView ;
 
     public FragmentTrainingButton1( ) {
 
@@ -30,34 +30,24 @@ public class FragmentTrainingButton1 extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 //        return inflater.inflate(R.layout.fragment_training_button_1 , container , false);
-       View view = inflater.inflate(R.layout.fragment_training_button_1 , container ,false);
-       mRecyclerView = view.findViewById(R.id.mRecyclerViewItemButton1);
-       mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        fakeData();
+       View view = inflater.inflate(R.layout.fragment_training_button_1 ,
+               container ,false);
+       ArrayList<Item> listItem = getListData();
+       listView = view.findViewById(R.id.lvButton1);
+       listView.setAdapter(new ListviewLessionAdapter(getActivity() , listItem));
+
        return view;
     }
 
-
-    private void fakeData(){
-        data = new ArrayList<>();
-        data.add("JUMPING PACK");
-        data.add("JUMPING PACK1");
-        data.add("JUMPING PACK2");
-        data.add("JUMPING PACK3");
-        data.add("JUMPING PACK4");
-        data.add("JUMPING PACK5");
-        data.add("JUMPING PACK5");
-        data.add("JUMPING PACK6");
-        data.add("JUMPING PACK7");
-
-//        mRecyclerView = new RecyclerView((Context) data);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerViewAdapter = new RecyclerViewAdapter(data);
-        mRecyclerView.setAdapter(mRecyclerViewAdapter);
-
-
-
-
+    private ArrayList<Item> getListData(){
+         ArrayList<Item> list = new ArrayList<Item>();
+        Item item = new Item();
+        item.setNameLession("jumpingjack");
+        item.setImageLession("img_jumpingjack");
+        item.setDurations(30);
+        list.add(item);
+        return list;
     }
+
+
 }
