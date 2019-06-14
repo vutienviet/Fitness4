d3a5953f8a5b5d255cc38e27d28e80b6a0ca3317
Envy <tranthanhtung1805@gmail.com>
2019-06-14 21:09:28 +0700

    Fix packkage

--- app/src/main/java/com/example/zeroc/fitness/ExViewActivity.java
+++ app/src/main/java/com/example/zeroc/fitness/ExViewActivity.java
@@ -1,16 +1,12 @@
 package com.example.zeroc.fitness;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.zeroc.fitness.model.Common;
 
 import Database.DatabaseExView;
 
@@ -37,43 +33,43 @@ public class ExViewActivity extends AppCompatActivity {
         detail_image = findViewById( R.id.detail_image );
 
         btnStart = findViewById( R.id.btnStart );
-        btnStart.setOnClickListener( new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // Timer
-                if (!isRunning) {
-                    btnStart.setText( "DONE" );
-
-                    int timeLimit = 0;
-                    // set TimeLimit
-                    if (workoutDB.getSettingMode() == 0) {
-                        timeLimit = Common.TIME_LIMIT_EASY;
-                    } else if (workoutDB.getSettingMode() == 1) {
-                        timeLimit = Common.TIME_LIMIT_MEDIUM;
-                    } else if (workoutDB.getSettingMode() == 2) {
-                        timeLimit = Common.TIME_LIMIT_HARD;
-                    }
-
-                    new CountDownTimer( timeLimit, 1000 ) {
-                        @Override
-                        public void onTick(long millisUntilFinished) {
-                            timer.setText( "" + millisUntilFinished / 1000 );
-                        }
-
-                        @Override
-                        public void onFinish() {
-                            Toast.makeText( ExViewActivity.this, "Finish!!", Toast.LENGTH_SHORT ).show();
-                            finish();
-                        }
-                    }.start();
-                } else {
-                    Toast.makeText( ExViewActivity.this, "Finish!!", Toast.LENGTH_SHORT ).show();
-                    finish();
-                }
-                isRunning = !isRunning;
-            }
-        } );
+//        btnStart.setOnClickListener( new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                // Timer
+//                if (!isRunning) {
+//                    btnStart.setText( "DONE" );
+//
+//                    int timeLimit = 0;
+//                    // set TimeLimit
+//                    if (workoutDB.getSettingMode() == 0) {
+//                        timeLimit = Common.TIME_LIMIT_EASY;
+//                    } else if (workoutDB.getSettingMode() == 1) {
+//                        timeLimit = Common.TIME_LIMIT_MEDIUM;
+//                    } else if (workoutDB.getSettingMode() == 2) {
+//                        timeLimit = Common.TIME_LIMIT_HARD;
+//                    }
+//
+//                    new CountDownTimer( timeLimit, 1000 ) {
+//                        @Override
+//                        public void onTick(long millisUntilFinished) {
+//                            timer.setText( "" + millisUntilFinished / 1000 );
+//                        }
+//
+//                        @Override
+//                        public void onFinish() {
+//                            Toast.makeText( ExViewActivity.this, "Finish!!", Toast.LENGTH_SHORT ).show();
+//                            finish();
+//                        }
+//                    }.start();
+//                } else {
+//                    Toast.makeText( ExViewActivity.this, "Finish!!", Toast.LENGTH_SHORT ).show();
+//                    finish();
+//                }
+//                isRunning = !isRunning;
+//            }
+//        } );
 
         timer.setText( "" );
         // get intent from ListExercise
