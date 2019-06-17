e1c3fa17b472138068fa3030bd06d31e48dd0ea6
zeroc <zerocute12@hotmail.com>
2019-06-18 00:39:08 +0700

    fixxxxxxxxx

--- app/src/main/java/com/example/zeroc/fitness/ExViewActivity.java
+++ app/src/main/java/com/example/zeroc/fitness/ExViewActivity.java
@@ -1,7 +1,13 @@
 package com.example.zeroc.fitness;
 
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,7 +17,10 @@ import com.bumptech.glide.Glide;
 import Database.DatabaseExView;
 
 public class ExViewActivity extends AppCompatActivity {
-
+    private static final int STATE_PAUSED = 0;
+    private static final int STATE_PLAYING = 1;
+    private int currentState;
+    private MediaBrowserCompat mediaBrowserCompat;
     int image_id;
     String name;
     TextView timer, title;
@@ -31,8 +40,26 @@ public class ExViewActivity extends AppCompatActivity {
         timer = findViewById( R.id.timer );
         title = findViewById( R.id.titleEx );
         detail_image = findViewById( R.id.detail_image );
+        mediaBrowserCompat = new MediaBrowserCompat(this, new ComponentName(this, BackgroundAudioService.class),
+                connectionCallback, getIntent().getExtras());
+        mediaBrowserCompat.connect();
 
         btnStart = findViewById( R.id.btnStart );
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( currentState == STATE_PAUSED ) {
+                    MediaControllerCompat.getMediaController(ExViewActivity.this).getTransportControls().play();
+                    currentState = STATE_PLAYING;
+                } else {
+                    if( MediaControllerCompat.getMediaController(ExViewActivity.this).getPlaybackState().getState() == PlaybackStateCompat.STATE_PLAYING ) {
+                        MediaControllerCompat.getMediaController(ExViewActivity.this).getTransportControls().pause();
+                    }
+
+                    currentState = STATE_PAUSED;
+                }
+            }
+        });
 //        btnStart.setOnClickListener( new View.OnClickListener() {
 //
 //            @Override
@@ -83,5 +110,45 @@ public class ExViewActivity extends AppCompatActivity {
 
         }
     }
+
+
+    private MediaControllerCompat.Callback mediaControllerCompatCallback = new MediaControllerCompat.Callback() {
+
+        @Override
+        public void onPlaybackStateChanged(PlaybackStateCompat state) {
+            super.onPlaybackStateChanged(state);
+            if( state == null ) {
+                return;
+            }
+
+            switch( state.getState() ) {
+                case PlaybackStateCompat.STATE_PLAYING: {
+                    currentState = STATE_PLAYING;
+                    break;
+                }
+                case PlaybackStateCompat.STATE_PAUSED: {
+                    currentState = STATE_PAUSED;
+                    break;
+                }
+            }
+        }
+    };
+   private MediaBrowserCompat.ConnectionCallback connectionCallback = new MediaBrowserCompat.ConnectionCallback() {
+
+        @Override
+        public void onConnected() {
+            super.onConnected();
+            try {
+                MediaControllerCompat mediaControllerCompat = new MediaControllerCompat(ExViewActivity.this, mediaBrowserCompat.getSessionToken());
+                mediaControllerCompat.registerCallback(mediaControllerCompatCallback);
+                MediaControllerCompat.setMediaController(ExViewActivity.this,mediaControllerCompat);
+                MediaControllerCompat.getMediaController(ExViewActivity.this).getTransportControls().playFromMediaId(String.valueOf(R.raw.dau_de_truong_thanh_onlyc),null);
+
+            } catch( RemoteException e ) {
+                e.printStackTrace();
+            }
+        }
+    };
+
 }
 
